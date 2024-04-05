f82d034526e4be5e1ae330aaac434466dae61e1d
vishnuKr006 <162488939+vishnuKr006@users.noreply.github.com>
2024-04-05 11:03:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let x = setTimeout (function () {
+    alert("Welcome To StudySync, 'Here we teach are Students to crack good placements & better opportunities in big MNC'S like Amazon, Google, Microsoft'.")
+}, 4500);
+
+let user = setTimeout (function() {
+    alert("Accept All Cookies")
+}, 7000)
